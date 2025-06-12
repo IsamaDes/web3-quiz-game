0263095c0f41fce6e76672fedd3d4c9a1d6e41e1
IsamaDes <isamadesmond@gmail.com>
2025-06-12 11:59:26 +0100

    display

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 // src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Quiz from './components/Quiz';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="min-h-screen bg-gray-100 p-4">
         <Routes>
-          <Route path="/" element={<Quiz />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
